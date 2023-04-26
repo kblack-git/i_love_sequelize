8e7938f7463f01cc2867c4157efb1b37e7ef810c
kblack-git <keithblack@gmail.com>
2023-04-26 09:35:50 -0400

    add product

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
+var morgan = require('morgan');
+// var logger = require('morgan');
+var logger = morgan('tiny');
+const helmet = require('helmet');
 var indexRouter = require('./routes/index');
 var orderRouter = require('./routes/order');
 var productRouter = require('./routes/product')
 
 var app = express();
 
-app.use(logger('dev'));
+
+// app.use(logger('dev'));
+app.use(helmet())
+app.use(logger)
+app.get('/', (req, res) => {
+    res.send('Hi');
+})
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
